bc79ae377282e8d961df4c5928fa9d93e987e1bb
Nitin <nsinghboss02@gmail.com>
2022-02-24 15:07:17 +0530

    "Question added"

--- src/com/Recursion/Main.java
+++ src/com/Recursion/Main.java
@@ -3,7 +3,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        /*
+        shelly has been given a stream of characters and she has to find first non repeating character each time a character is inserted to the stream.
+        Example:
+        Input : aabc
+        Output : a -1 b b
+        Input Format
+        a-z lowercase characters
+
+        Constraints
+        Should be memory efficient
+        Output Format
+        print -1 if queue is empty or print first non repeating character.
+
+        Sample Input 0
+
+        aac
+        Sample Output 0
+        a -1 c
+         */
+
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         firstNonRepeat(str);
